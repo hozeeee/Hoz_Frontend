66d6fc8d3f4029b9c51af7a879276545ec49a5b8
hozee <15820676504@139.com>
2021-02-15 23:45:07 +0800

    Egg

---
+++ Egg/egg_demo/app/middleware/gzip.ts
@@ -0,0 +1,28 @@
+// import { EggLoaderOptions } from 'egg';
+import isJSON from "koa-is-json";
+import zlib = require("zlib");
+
+// 中间件的使用 和 options 的参数都在 config.default.js 中配置
+module.exports = (options) => {
+  // 与 Koa 的洋葱模型一致，必须是一个异步函数
+  return async function gzip(ctx, next) {
+    await next();
+
+    // 后续中间件执行完成后将响应体转换成 gzip
+    let body = ctx.body;
+
+    if (!body) return;
+    if (options.threshold && ctx.length < options.threshold) return;
+    if (isJSON(body)) body = JSON.stringify(body);
+
+    // 设置 gzip body，修正响应头
+    const stream = zlib.createGzip();
+    stream.end(body);
+    ctx.body = stream;
+    ctx.set('Content-Encoding', 'gzip');
+  };
+};
+
+
+
+
